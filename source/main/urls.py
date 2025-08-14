84b1a1475f413c09bf05d78c509ccb374c29c43b
prodmodfour <prodmodfour@gmail.com>
2025-08-15 00:41:40 +0100

    Added dummy display module catalogue

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,26 +1,20 @@
-"""
-URL declarations for the main app.
 
-These routes correspond to the mock interface's pages. The ``home``
-view renders the landing page with the dataset cards, and the
-``analysis`` view renders a generic analysis page whose heading changes
-based on the selected data source. Additional routes can be added
-easily as the mock evolves.
-"""
 
-from django.urls import path  # type: ignore
+from django.urls import path  
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('analysis/<str:source>/', views.analysis, name='analysis'),
-    # Route for selecting an instrument when creating a new workspace.
-    # This page displays a grid of available instruments whose
-    # contents vary depending on the selected data source (e.g. ISIS,
-    # CLF or Diamond).
+
     path('analysis/<str:source>/instruments/', views.instruments, name='instruments'),
 
     path('analysis/<str:source>/instruments/<str:instrument>/', 
          views.instrument_detail, 
          name='instrument_detail'),
+
+
+    path('data-display-catalogue/',
+        views.data_display_catalogue,
+        name='data_display_catalogue'),
 ]
