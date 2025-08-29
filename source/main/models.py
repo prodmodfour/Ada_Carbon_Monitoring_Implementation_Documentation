050bd27398df14b7be87214681dc6c975d81c916
prodmodfour <prodmodfour@gmail.com>
2025-08-29 12:02:42 +0100

    Decided on new model structure

--- source/main/models.py
+++ source/main/models.py
@@ -1,9 +1,22 @@
-
-
 from django.db import models
-from django.db.models import Q, F
-from django.utils import timezone
-import uuid
+
+# Usage Models
+
+class DayUsage(models.Model)
+    
+class MonthUsage(models.Model):
+
+class YearUsage(models.Model):
+
+class ProjectUsage(models.Model):
+
+
+class WorkspaceUsage(models.Model):
+
+
+
+class InstrumentAverageUsage(models.Model):
+
 
 
 
