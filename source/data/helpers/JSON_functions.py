731b786355e310e682ef3a7b7ce17735cd1ddd51
prodmodfour <prodmodfour@gmail.com>
2025-09-12 12:23:24 +0100

    Removed unnecessary timestamp sectins of functions as they are now in a helper function

--- source/data/helpers/JSON_functions.py
+++ source/data/helpers/JSON_functions.py
@@ -11,29 +11,18 @@ def construct_estimated_project_usage_file_path(cloud_project_name, timestamp):
     return "data/estimated_project_usage/" + cloud_project_name + "/" + str(year) + "/" + str(month) + "/" + str(day) + "/" + str(hour) + ".json"
 
 def save_estimated_project_usage_entry(cloud_project_name, entry):
-    # Get Year, Month, Day, hour from datetime object
-    year = entry.timestamp.year
-    month = entry.timestamp.month
-    day = entry.timestamp.day
-    hour = entry.timestamp.hour
-    # Construct filepath
-    file_path = construct_estimated_project_usage_file_path(cloud_project_name, timestamp)
+    file_path = construct_estimated_project_usage_file_path(cloud_project_name, entry.timestamp)
 
 
     # Create the file if it doesn't exist
     if not os.path.exists(file_path):
-        os.makedirs(subfolder)
+        os.makedirs(os.path.dirname(file_path))
 
     # Save the entry to the file
-    with open(file, "w") as f:
+    with open(file_path, "w") as f:
         f.write(entry.construct_json())
 
 def load_estimated_project_usage_entry(cloud_project_name, timestamp):
-    # Get Year, Month, Day, hour from datetime object
-    year = timestamp.year
-    month = timestamp.month
-    day = timestamp.day
-    hour = timestamp.hour
     # Construct filepath
     file_path = construct_estimated_project_usage_file_path(cloud_project_name, timestamp)
 
@@ -46,7 +35,7 @@ def load_estimated_project_usage_entry(cloud_project_name, timestamp):
 
     # Construct an EstimatedUsageEntry from the json
     entry = EstimatedUsageEntry()
-    entry.set_timestamp(timestamp)
+    entry.set_timestamp(json["timestamp"])
     entry.set_cpu_seconds_total(json["busy_cpu_seconds_total"], json["idle_cpu_seconds_total"])
     entry.set_usage_kwh(json["busy_usage_kwh"], json["idle_usage_kwh"])
     entry.set_usage_gCO2eq(json["busy_usage_gCO2eq"], json["idle_usage_gCO2eq"])
