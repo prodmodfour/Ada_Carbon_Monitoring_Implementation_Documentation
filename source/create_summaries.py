e56f1266379247e13d31646983aedc13dbf3c6aa
prodmodfour <prodmodfour@gmail.com>
2025-09-23 23:36:10 +0100

    Added summarise_machine_day function

--- source/create_summaries.py
+++ source/create_summaries.py
@@ -71,12 +71,12 @@ def _summarise_project_day(base_file_path: str, project_label: str):
 
 
 def _summarise_machines_day(base_file_path: str, project_label: str):
-    file_path = f"{base_file_path}{project_label}_machine_timeseries.json"
+    file_path = f"{base_file_path}{project_label}_timeseries.json"
     data = None
+  
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
-
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except json.JSONDecodeError:
@@ -85,6 +85,54 @@ def _summarise_machines_day(base_file_path: str, project_label: str):
         print("Permission error")
     except OSError:
         print("OS Error")
+
+    if data == None:
+        return
+  
+    busy_kwh = list()
+    idle_kwh = list()
+    intensities = list()
+    busy_gCo2eq = list()
+    idle_gCo2eq = list()
+
+    machine_totals = dict()
+    for timestamp, machine_data in data.items():
+        for machine_name, values in machine_data:
+            if machine_name not in machine_totals.keys():
+                machine_totals[machine_name] = {
+                    "busy_kwh" = list(),
+                    "idle_kwh" = list(),
+                    "intensities" = list(),
+                    "busy_gCo2eq" = list(),
+                    "idle_gCo2eq" = list(),
+                }
+            machine_totals[machine_name]["busy_kwh"].append(values["busy_kwh"])
+            machine_totals[machine_name]["idle_kwh"].append(values["idle_kwh"])
+            machine_totals[machine_name]["indensities"].append(values["intensity_gCo2eq/kwh"])
+            machine_totals[machine_name]["busy_gCo2eq"].append(values["busy_gCo2eq"])
+            machine_totals[machine_name]["idle_gCo2eq"].append(values["idle_gCo2eq"])
+
+
+    for machine_name in machine_totals.keys():
+        sum_busy_kwh = sum(machine_totals[machine_name]["busy_kwh"])
+        sum_idle_kwh = sum(machine_totals[machine_name]["idle_kwh"])
+        sum_intensities = sum(machine_totals[machine_name]["intensities"])
+        sum_busy_gCo2eq = sum(machine_totals[machine_name]["busy_gCo2eq"])
+        sum_idle_gCo2eq = sum(machine_totals[machine_name]["idle_gCo2eq"])
+        summary = {
+            "total_busy_kwh": sum_busy_kwh,
+            "total_idle_kwh": sum_idle_kwh,
+            "average_intensity_gCo2eq/kwh": sum_intensities / len(sum_intensities),
+            "total_busy_gCo2eq": sum_busy_gCo2eq,
+            "total_idle_gCo2eq": sum_idle_gCo2eq,
+            "idle_percentage_kwh": (sum_idle_kwh / (sum_busy_kwh + sum_idle_kwh) * 100),
+            "idle_percentage_gCo2eq": (sum_idle_gCo2eq / (sum_busy_gCo2eq + sum_idle_gCo2eq) * 100)
+        }
+
+        save_file_path = f"{base_file_path}{project_label}_{machine_name}_machine_summary.json"
+
+        with open(save_file_path, 'w') as json_file:
+            json.dump(summary, json_file, indent=4)
 
     
 
