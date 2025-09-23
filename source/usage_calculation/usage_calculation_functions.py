965eef764671e3f7f42edf6356b19ccd28070b9c
prodmodfour <prodmodfour@gmail.com>
2025-09-23 17:36:11 +0100

    Added type hints

--- source/usage_calculation/usage_calculation_functions.py
+++ source/usage_calculation/usage_calculation_functions.py
@@ -1,10 +1,11 @@
+import typing
 
-def estimate_electricity_usage_kwh(cpu_seconds_total, cpu_tdp_w):
+def estimate_electricity_usage_kwh(cpu_seconds_total: float, cpu_tdp_w: float) -> float:
     # Calculate the usage in kWh
     usage_kwh = cpu_seconds_total * cpu_tdp_w / 1000
     return usage_kwh
 
-def estimate_carbon_footprint_gCO2eq(usage_kwh, ci_g_per_kwh):
+def estimate_carbon_footprint_gCO2eq(usage_kwh: float, ci_g_per_kwh: float) -> float:
     # Calculate the carbon footprint in gCO2eq
     carbon_footprint_gCO2eq = usage_kwh * ci_g_per_kwh
     return carbon_footprint_gCO2eq
