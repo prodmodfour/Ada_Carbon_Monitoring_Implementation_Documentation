ac713240c523f3687d9aec1d2a78a0e4758e888a
prodmodfour <prodmodfour@users.noreply.github.com>
2026-01-28 13:37:50 +0000

    docs: remove source directory

--- source/mongodb/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-MongoDB module for Ada Carbon Monitoring
-"""
-from .MongoDBClient import MongoDBClient
-
-__all__ = ['MongoDBClient']
--- source/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Unit tests for Ada Carbon Monitoring Implementation
-"""
